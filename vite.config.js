184a62e3547d8b90cd3b902e62fca92a8939f6e7
abdo <abdonsila222@gmail.com>
2023-09-19 21:48:57 +0100

    UPDATE vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
+  base: "/The-Prayer-Project/"
 
 })
