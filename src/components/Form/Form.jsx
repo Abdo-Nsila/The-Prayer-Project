e835f9586bdd8e49e4013d2873d449b181ae16a5
No0ne003 <hi.yassir.op@gmail.com>
2024-05-25 12:47:00 +0100

    style: improving forms look

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import Table from "../Table/TableData";
 
+// component
 import { CountrySelect, StateSelect } from "react-country-state-city";
 import "react-country-state-city/dist/react-country-state-city.css";
 import ReactLoading from "react-loading";
-
-import "./Form.css";
 
 export default function App() {
   const [countryid, setCountryid] = useState(0);
@@ -13,24 +12,31 @@ export default function App() {
   const [cityName, setcityName] = useState("");
 
   return (
-    <>
-      <form>
-        <h6>Country</h6>
-        <CountrySelect
-          onChange={(e) => {
-            setCountryid(e.id);
-            setCountryName(e.name);
-          }}
-          placeHolder="Select Country"
-        />
-        <h6>State</h6>
-        <StateSelect
-          countryid={countryid}
-          onChange={(e) => {
-            setcityName(e.name);
-          }}
-          placeHolder="Select City"
-        />
+    <div className="w-full container mx-auto">
+      <form className="w-full flex justify-center items-center">
+        <div className="w-[500px]">
+          <div className="flex flex-col py-5 gap-2">
+            <h6 className="text-3xl text-neutral-200">Country</h6>
+            <CountrySelect
+              onChange={(e) => {
+                setCountryid(e.id);
+                setCountryName(e.name);
+              }}
+              placeHolder="Select Country"
+            />
+          </div>
+
+          <div className="flex flex-col py-5">
+            <h6 className="text-3xl text-neutral-200">State</h6>
+            <StateSelect
+              countryid={countryid}
+              onChange={(e) => {
+                setcityName(e.name);
+              }}
+              placeHolder="Select City"
+            />
+          </div>
+        </div>
       </form>
 
       {countryName && cityName ? (
@@ -40,7 +46,7 @@ export default function App() {
           <ReactLoading type="spinningBubbles" color="#fff" />
         </div>
       )}
-    </>
+    </div>
   );
 }
 
