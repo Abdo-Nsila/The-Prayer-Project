32e635866f78d7e9b44366a80d0c62a65120ab8b
No0ne003 <hi.yassir.op@gmail.com>
2024-05-29 17:32:35 +0100

    refactore: remove not needing imports

--- tests/components/Form.test.jsx
+++ tests/components/Form.test.jsx
@@ -1,4 +1,3 @@
-import userEvent from "@testing-library/user-event";
 import { render, fireEvent } from "@testing-library/react";
 import Form from "../../src/components/Form/Form";
 
--- tests/components/TableData.test.jsx
+++ tests/components/TableData.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render  } from "@testing-library/react";
 import TableData from "../../src/components/Table/TableData";
 
 describe("TableData", () => {
